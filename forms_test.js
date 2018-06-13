04897301dd424e413c4c77f64a48ad61f335cd8d
ARVENTO\volkan.erleblebici <volkan.erleblebici@gmail.com>
2018-06-13 19:55:32 +0300

    İlk çalisir kod tamamlandi. Test1 ilk 2 kisim tamamlandi. POM yapisi uygulanacak.

--- forms_test.js
+++ forms_test.js
@@ -1,12 +1,45 @@
-Feature('Forms');
+Feature('Udemy');
 
-Scenario('test something', (I) => {
-  I.amOnPage('/');
-  I.fillField('#searchText', 'hello@world.com');
-  //I.fillField('Password', '123456');
-  I.checkOption('Active');
-  I.checkOption('Male');  
-  I.click('Create User');
-  I.see('User is valid');
-  I.dontSeeInCurrentUrl('/documentation');  
+Scenario('test1', async (I) => {
+  I.amOnPage('https://www.sahibinden.com');
+ 
+  /*
+    Test1 1. asamada yapilmasi istenen gorunurluk islemleri...
+  */
+  //logo gorunurlugu kontrolu...
+  I.seeElement('/html/body/div[3]/div/p/a');
+  //Arama alani gorunurlugu kontrolu...
+  I.seeElement('#searchText');
+  //Giris Yap link gorunurlugu kontrolu...
+  I.seeElement('/html/body/div[3]/div/ul/li[1]/a');
+  //Uye Ol link gorunurlugu kontrolu...
+  I.seeElement('/html/body/div[3]/div/ul/li[2]/a');
+  //Ucretsiz Ilan Ver buton gorunurlugu kontrolu...
+  I.seeElement('#post-new-classified');
+  // Wait gerekli degil deneme amacli...
+  I.wait(1);
+  //Detayli Arama link gorunurlugu kontrolu...
+  I.seeElement('#searchSuggestionForm a');
+  //Ucretsiz Ilan Ver buton gorunurlugu kontrolu...
+  I.seeElement('//*[@id="container"]/div[3]/div/div[2]/div[3]/h3');
+
+  /*
+    Test1 2. asamada yapilmasi istenen gorunurluk ve 56 resimli
+    ilan varliginin kontrol islemleri...
+  */
+
+  //vitrin degiskeni tanimlaniyor ve verilen locatordan text degeri alinarak bu degiskene set ediliyor...
+  var vitrin = await I.grabTextFrom('//*[@id="container"]/div[3]/div/div[2]/div[3]/h3');
+  //Alinan degerin consolda görülmesi islemleri...
+  console.log(`Anasayfa Vitrini olarak deger : ${vitrin}`);
+  I.say(`Anasayfa Vitrini olarak deger : ${vitrin}`);
+
+  //links degiskeni tanimlaniyor ve verilen locatordan kac adet bulundugu bu degiskene set ediliyor...
+  var links = await I.grabNumberOfVisibleElements('//*[@id="container"]/div[3]/div/div[2]/div[3]/ul/li');
+  // Wait gerekli degil deneme amacli...
+  I.wait(2);
+  //Alinan degerin consolda görülmesi islemleri...
+  console.log(`Anasayfa Vitrini altindaki resimli ilan sayisi : ${links}`);
+  I.say(`Anasayfa Vitrini altindaki resimli ilan sayisi : ${links}`);
+  
 });
